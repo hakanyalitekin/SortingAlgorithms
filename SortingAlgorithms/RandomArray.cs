cc3973b05391c2db4db96488328e9eb9f3cbc705
biproberkay <biproberkay@gmail.com>
2022-01-11 00:04:05 +0300

    randomArray.cs Generator() ekledim. *enter*a basınca 0 algıla!sındiye tryparse kullandım. bir de isimlendirmede bi hata vardı sort yerine short yazılmış sanırım onu düzelttim.

---
+++ SortingAlgorithms/RandomArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    internal static class RandomArray
+    {
+        internal static int[] Generate(int arrLength, int arrMax)
+        {
+
+            Random rnd = new Random();
+            int[] test = new int[rnd.Next(arrLength)];
+            for (int i = 0; i < test.Length; i++)
+            {
+                test[i] = rnd.Next(arrMax);
+                Console.WriteLine(test[i]);
+            }
+            return test;
+        }
+    }
+}
